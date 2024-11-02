f9f7459b9133a1e0c76de3b878eefbe260824b7b
naure <naure@users.noreply.github.com>
2024-11-02 07:56:23 +0100

    Gadget to assert non-zero and non-equal (#535)
    Co-authored-by: Aurélien Nicolas <info@nau.re>

--- ceno_zkvm/src/gadgets/is_zero.rs
+++ ceno_zkvm/src/gadgets/is_zero.rs
@@ -11,13 +11,13 @@ use crate::{
 };
 
 pub struct IsZeroConfig {
-    is_zero: WitIn,
+    is_zero: Option<WitIn>,
     inverse: WitIn,
 }
 
 impl IsZeroConfig {
     pub fn expr<E: ExtensionField>(&self) -> Expression<E> {
-        self.is_zero.expr()
+        self.is_zero.map(|wit| wit.expr()).unwrap_or(0.into())
     }
 
     pub fn construct_circuit<E: ExtensionField, NR: Into<String>, N: FnOnce() -> NR>(
@@ -25,15 +25,38 @@ impl IsZeroConfig {
         name_fn: N,
         x: Expression<E>,
     ) -> Result<Self, ZKVMError> {
+        Self::construct(cb, name_fn, x, false)
+    }
+
+    pub fn construct_non_zero<E: ExtensionField, NR: Into<String>, N: FnOnce() -> NR>(
+        cb: &mut CircuitBuilder<E>,
+        name_fn: N,
+        x: Expression<E>,
+    ) -> Result<Self, ZKVMError> {
+        Self::construct(cb, name_fn, x, true)
+    }
+
+    fn construct<E: ExtensionField, NR: Into<String>, N: FnOnce() -> NR>(
+        cb: &mut CircuitBuilder<E>,
+        name_fn: N,
+        x: Expression<E>,
+        assert_non_zero: bool,
+    ) -> Result<Self, ZKVMError> {
         cb.namespace(name_fn, |cb| {
-            let is_zero = cb.create_witin(|| "is_zero");
+            let (is_zero, is_zero_expr) = if assert_non_zero {
+                (None, 0.into())
+            } else {
+                let is_zero = cb.create_witin(|| "is_zero");
+
+                // x!=0 => is_zero=0
+                cb.require_zero(|| "is_zero_0", is_zero.expr() * x.clone())?;
+
+                (Some(is_zero), is_zero.expr())
+            };
             let inverse = cb.create_witin(|| "inv");
 
             // x==0 => is_zero=1
-            cb.require_one(|| "is_zero_1", is_zero.expr() + x.clone() * inverse.expr())?;
-
-            // x!=0 => is_zero=0
-            cb.require_zero(|| "is_zero_0", is_zero.expr() * x.clone())?;
+            cb.require_one(|| "is_zero_1", is_zero_expr + x.clone() * inverse.expr())?;
 
             Ok(IsZeroConfig { is_zero, inverse })
         })
@@ -50,7 +73,9 @@ impl IsZeroConfig {
             (F::ZERO, x.invert().expect("not zero"))
         };
 
-        set_val!(instance, self.is_zero, is_zero);
+        if let Some(wit) = self.is_zero {
+            set_val!(instance, wit, is_zero);
+        }
         set_val!(instance, self.inverse, inverse);
 
         Ok(())
@@ -71,6 +96,19 @@ impl IsEqualConfig {
         b: Expression<E>,
     ) -> Result<Self, ZKVMError> {
         Ok(IsEqualConfig(IsZeroConfig::construct_circuit(
+            cb,
+            name_fn,
+            a - b,
+        )?))
+    }
+
+    pub fn construct_non_equal<E: ExtensionField, NR: Into<String>, N: FnOnce() -> NR>(
+        cb: &mut CircuitBuilder<E>,
+        name_fn: N,
+        a: Expression<E>,
+        b: Expression<E>,
+    ) -> Result<Self, ZKVMError> {
+        Ok(IsEqualConfig(IsZeroConfig::construct_non_zero(
             cb,
             name_fn,
             a - b,
