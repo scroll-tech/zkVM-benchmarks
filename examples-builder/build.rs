79fa76ea438d7e05e049c56d2c00f6b2f04c597a
Matthias Görgens <matthias.goergens@gmail.com>
2024-12-16 11:08:05 +0800

    Flatten crate structure again (#754)
    Remove the workspace for RiscV crates, and treat them as individuals.
    
    See
    https://github.com/scroll-tech/ceno/pull/631#pullrequestreview-2501650283
    for background on the request.
    
    Extracted from https://github.com/scroll-tech/ceno/pull/631

--- examples-builder/build.rs
+++ examples-builder/build.rs
@@ -26,7 +26,7 @@ fn build_elfs() {
     // See git history for an attempt to do this.
     let output = Command::new("cargo")
         .args(["build", "--release", "--examples"])
-        .current_dir("../guest/examples")
+        .current_dir("../examples")
         .env_clear()
         .envs(std::env::vars().filter(|x| !x.0.starts_with("CARGO_")))
         .output()
@@ -41,13 +41,12 @@ fn build_elfs() {
             dest,
             r#"#[allow(non_upper_case_globals)]
             pub const {example}: &[u8] =
-                include_bytes!(r"{CARGO_MANIFEST_DIR}/../guest/target/riscv32im-unknown-none-elf/release/examples/{example}");"#
+                include_bytes!(r"{CARGO_MANIFEST_DIR}/../examples/target/riscv32im-unknown-none-elf/release/examples/{example}");"#
         ).expect("failed to write vars.rs");
     }
-    let input_path = "../guest/";
-    let elfs_path = "../guest/target/riscv32im-unknown-none-elf/release/examples/";
-
-    println!("cargo:rerun-if-changed={input_path}");
+    println!("cargo:rerun-if-changed=../examples/");
+    println!("cargo:rerun-if-changed=../ceno_rt/");
+    let elfs_path = "../examples/target/riscv32im-unknown-none-elf/release/examples/";
     println!("cargo:rerun-if-changed={elfs_path}");
 }
 
